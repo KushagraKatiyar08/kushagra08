140afdefd14afc9aadf9444192f9cd42150c6073
Kushagra Katiyar <kushagrakatiyar08@gmail.com>
2025-11-21 12:50:04 +0530

    Add Employee struct and input/output functionality
    Displaying Employee data using structure in C programming language.

---
+++ Main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct Employee {
+    char name[50];
+    int id;
+    float salary;
+};
+
+int main() {
+    struct Employee emp;
+
+    printf("Enter Employee Name: ");
+    scanf("%s", emp.name);
+
+    printf("Enter Employee ID: ");
+    scanf("%d", &emp.id);
+
+    printf("Enter Employee Salary: ");
+    scanf("%f", &emp.salary);
+
+    printf("\n--- Employee Details ---\n");
+    printf("Name   : %s\n", emp.name);
+    printf("ID     : %d\n", emp.id);
+    printf("Salary : %.2f\n", emp.salary);
+
+    return 0;
+}
